2708c8f32e1b3c67259c125382bbea9bc1f99729
zxc87125@gmail.com <zxc87125@mail.com>
2021-07-29 19:56:38 +0800

    Initial commit

---
+++ app/src/main/java/com/tw/ch/lcc/hankchan/menudemo/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.tw.ch.lcc.hankchan.menudemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private Button endbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        endbtn = findViewById(R.id.endbtn);
+        endbtn.setOnClickListener(v->{
+            finish();  // 關閉此頁
+        });
+    }
+}
